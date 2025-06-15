0033db917cd16813c77f55cfd9677a5107193c23
wonappy <wonappy@naver.com>
2025-06-15 18:47:49 +0900

    강의 미리보기 (자막 only 창) 연결 완료

--- lib/screens/subtitles_only_screen.dart
+++ lib/screens/subtitles_only_screen.dart
@@ -49,44 +49,62 @@ class _SubtitlesOnlyScreenState extends State<SubtitlesOnlyScreen> {
     final double screenHeight = screenSize.height;
 
     return Scaffold(
-      body: Container(
-        width: screenWidth,
-        height: screenHeight,
-        color: widget.backgroundColor, //배경 색상 지정
-        child: Column(
-          children: [
-            for (int i = 0; i < widget.languages.length; i++)
-              Column(
-                children: [
-                  SizedBox(height: widget.subSpacing), //자막 간 간격 지정
-                  Container(
-                    padding: EdgeInsets.symmetric(horizontal: 10, vertical: 10),
-                    width: screenWidth * 0.95,
-                    constraints: BoxConstraints(
-                      maxHeight: screenHeight * 0.5,
-                    ), //최대 자막 컨테이너 높이
-                    decoration: BoxDecoration(
-                      color: widget.subBackgroundColor.withValues(
-                        //자막 배경 색상 지정
-                        alpha: widget.opacitySubBackground, //자막 배경 불투명도 지정
+      body: Stack(
+        children: [
+          Container(
+            width: screenWidth * 0.9,
+            height: screenHeight,
+            color: widget.backgroundColor, //배경 색상 지정
+            child: Column(
+              children: [
+                for (int i = 0; i < widget.languages.length; i++)
+                  Column(
+                    children: [
+                      SizedBox(height: widget.subSpacing), //자막 간 간격 지정
+                      Container(
+                        padding: EdgeInsets.symmetric(
+                          horizontal: 10,
+                          vertical: 10,
+                        ),
+                        width: screenWidth * 0.95,
+                        constraints: BoxConstraints(
+                          maxHeight: screenHeight * 0.5,
+                        ), //최대 자막 컨테이너 높이
+                        decoration: BoxDecoration(
+                          color: widget.subBackgroundColor.withValues(
+                            //자막 배경 색상 지정
+                            alpha: widget.opacitySubBackground, //자막 배경 불투명도 지정
+                          ),
+                          borderRadius: BorderRadius.circular(20),
+                        ),
+                        child: Text(
+                          widget.languages[i].subtitle, //자막 내용 지정
+                          textAlign: TextAlign.center,
+                          style: TextStyle(
+                            color: widget.subWordColor, //자막 글자 색상 지정
+                            fontSize: widget.subWordFontSize, //자막 글자 크기 지정
+                            fontWeight: FontWeight.w700,
+                            textBaseline: null,
+                          ),
+                        ),
                       ),
-                      borderRadius: BorderRadius.circular(20),
-                    ),
-                    child: Text(
-                      widget.languages[i].subtitle, //자막 내용 지정
-                      textAlign: TextAlign.center,
-                      style: TextStyle(
-                        color: widget.subWordColor, //자막 글자 색상 지정
-                        fontSize: widget.subWordFontSize, //자막 글자 크기 지정
-                        fontWeight: FontWeight.w700,
-                        textBaseline: null,
-                      ),
-                    ),
+                    ],
                   ),
-                ],
-              ),
-          ],
-        ),
+              ],
+            ),
+          ),
+          Positioned(
+            bottom: 16,
+            right: 16,
+            child: IconButton(
+              onPressed: () {
+                Navigator.pop(context);
+              },
+              icon: const Icon(Icons.close_rounded),
+              iconSize: 32,
+            ),
+          ),
+        ],
       ),
     );
   }
