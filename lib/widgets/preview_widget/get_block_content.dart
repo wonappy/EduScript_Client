21c8968ac82a7210bae655e23a20af6823d71ef1
wonappy <wonappy@naver.com>
2025-11-18 10:15:09 +0900

    style 오버레이 모드 자막 미리보기 창 생성
    화면 공유가 On이 되었을 때, 오버레이 모드의 자막 미리보기 화면이 활성화될 수 있도록 변경함.

--- lib/widgets/preview_widget/get_block_content.dart
+++ lib/widgets/preview_widget/get_block_content.dart
@@ -2,9 +2,12 @@
 library;
 
 import 'package:client/widgets/preview_widget/play_progress/build_lecture_playbar_content.dart';
+import 'package:client/widgets/preview_widget/preview_screen/buil_lecture_preview_overlay_content.dart';
 import 'package:client/widgets/preview_widget/preview_screen/build_lecture_preview_content.dart';
 import 'package:client/widgets/preview_widget/subtitle_setting/build_subtitle_setting_content.dart';
+import 'package:client/widgets/preview_widget/subtitle_setting_provider.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 class GetBlockContent extends StatelessWidget {
   final String title;
@@ -20,9 +23,17 @@ class GetBlockContent extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final subtitleSettings = context.watch<SubtitleSettingsProvider>();
+
     switch (title) {
       case '화면 미리보기':
-        return BuildLecturePreviewContent();
+        if (subtitleSettings.screenSharedEnabled) {
+          //오버레이 모드
+          return BuildLecturePreviewOverlayContent();
+        } else {
+          //기본 자막창 모드
+          return BuildLecturePreviewContent();
+        }
       case '':
         return BuildLecturePlayBarContent(
           screenWidth: screenWidth,
---
+++ lib/widgets/preview_widget/preview_screen/buil_lecture_preview_overlay_content.dart
@@ -0,0 +1,85 @@
+/// 강의 화면 미리보기 (오버레이 모드) 컨텐츠
+library;
+
+import 'package:client/core/styles/color_core.dart';
+import 'package:flutter/material.dart';
+
+import '../subtitle_setting_provider.dart';
+import 'package:provider/provider.dart';
+
+class BuildLecturePreviewOverlayContent extends StatelessWidget {
+  const BuildLecturePreviewOverlayContent({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final settings = context.watch<SubtitleSettingsProvider>();
+    final languages = settings.selectedOutputLanguages;
+
+    const double referenceScreenWidth = 800.0;
+
+    return LayoutBuilder(
+      builder: (context, constraints) {
+        final screenWidth = constraints.maxWidth;
+        final screenHeight = constraints.maxHeight;
+
+        final double scaleFactor = screenWidth / referenceScreenWidth;
+
+        return Container(
+          width: screenWidth,
+          height: screenHeight,
+          decoration: BoxDecoration(
+            gradient: LinearGradient(
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
+              colors: [Colors.blue[200]!, Colors.purple[100]!],
+            ), //배경 색상 지정 (그라데이션 -> 예시 바탕화면)
+            borderRadius: BorderRadius.circular(8),
+          ),
+          padding: EdgeInsets.symmetric(
+            horizontal: 0.05 * screenWidth,
+            vertical: 20 * scaleFactor,
+          ),
+          //자막 설정
+          child: Column(
+            mainAxisAlignment: settings.getAlignment(),
+            crossAxisAlignment: CrossAxisAlignment.start, // 왼쪽 정렬 지정
+            children: [
+              for (int i = 0; i < languages.length; i++)
+                Padding(
+                  padding: EdgeInsets.only(bottom: 10 * scaleFactor),
+                  child: Container(
+                    padding: EdgeInsets.symmetric(
+                      horizontal: 10 * scaleFactor,
+                      vertical: 7 * scaleFactor,
+                    ),
+                    constraints: BoxConstraints(
+                      maxWidth: screenWidth * 0.95,
+                      maxHeight: screenHeight * 0.2,
+                    ),
+                    // 최대 자막 컨테이너 높이
+                    decoration: BoxDecoration(
+                      color: Colors.black, //자막 배경 색상 ; 검정색 지정
+                      borderRadius: BorderRadius.circular(5),
+                    ),
+                    child: Text(
+                      settings.getPreviewText(languages[i]), // 자막 내용 지정
+                      textAlign: TextAlign.left, // 자막 왼쪽 정렬
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize:
+                            settings.getFontSize(screenWidth) *
+                            scaleFactor, //자막 글자 크기 지정
+                        fontStyle: settings.getFontStyle(),
+                        height: 1.2,
+                        textBaseline: null,
+                      ),
+                    ),
+                  ),
+                ),
+            ],
+          ),
+        );
+      },
+    );
+  }
+}
