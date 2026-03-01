27a3ce01efb4ba90c3ab937e1f555121d2ff959d
suhyeon <102345466+jeong-su-hyeon@users.noreply.github.com>
2026-03-01 20:54:53 +0900

    refactor: 스타일 옵션 core 추가
    기존엔 자막 설정에서 드롭다운에 options 값을 하드코딩해서 전달했었는데, style_option_core에서 관리하도록 함

---
+++ lib/core/styles/style_option_core.dart
@@ -0,0 +1,11 @@
+
+/// ### 자막 설정 옵션 (리스트 관리)
+class StyleOptionCore {
+  static const List<String> positionOptions = ["상단", "중앙", "하단"];
+  static const List<String> horizontalPositionOptions = ["좌측", "중앙", "우측"];
+  static const List<String> fontStyleOptions = ["기본", "굵게", "이탤릭"];
+  static const List<String> fontSizeOptions = ["매우 작게", "작게", "중간", "크게", "매우 크게"];
+  static const List<String> fontColorOptions = ["빨강", "주황", "노랑", "초록", "파랑", "보라", "검정", "흰색"];
+  static const List<String> backgroundColorOptions = ["빨강", "주황", "노랑", "초록", "파랑", "보라", "검정", "흰색"];
+  static const List<String> backgroundOpacityOptions = ["0%", "25%", "50%", "75%", "100%"];
+}
